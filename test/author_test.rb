a90d54cda91f55c9bdab4da8e86f760194029914
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-22 13:01:06 -0600

    Update AuthorTest to include test for attributes

--- test/author_test.rb
+++ test/author_test.rb
@@ -10,4 +10,13 @@ class AuthorTest < Minitest::Test
     assert_instance_of Author, charlotte_bronte
   end
 
+  def test_it_has_attributes
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    assert_equal "Charlotte Bronte", charlotte_bronte.name
+    assert_equal [], charlotte_bronte.books
+  end
+
+  
+
 end

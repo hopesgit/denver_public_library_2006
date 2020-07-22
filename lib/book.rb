275a1bc7f447b77ba721f7400bb8a279508866de
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-22 12:56:11 -0600

    Update Book to be more readable

--- lib/book.rb
+++ lib/book.rb
@@ -5,8 +5,8 @@ class Book
               :publication_year
 
   def initialize(details)
-    @author = details[:author_first_name] + " " + details[:author_last_name]
-    @title = details[:title]
+    @author           = details[:author_first_name] + " " + details[:author_last_name]
+    @title            = details[:title]
     @publication_year = details[:publication_date][-4..-1]
   end
 end

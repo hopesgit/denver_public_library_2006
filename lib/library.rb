c74bbb4d876fbef09dfa528d30747eea264c3991
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-22 14:28:57 -0600

    Add method and test for Library: 'publication_time_frame_for(author)'

--- lib/library.rb
+++ lib/library.rb
@@ -21,4 +21,18 @@ class Library
     end
     @books
   end
+
+  def find_author(author_to_look_for)
+    @authors.index(author_to_look_for)
+  end
+
+  def publication_time_frame_for(author_to_look_for)
+    index_no = find_author(author_to_look_for)
+    @authors[index_no].books.min_by {|book| book.publication_year}
+    date_range = {
+    start: @authors[index_no].books.first.publication_year,
+    end: @authors[index_no].books.last.publication_year
+    }
+    date_range
+  end
 end
--- test/library_test.rb
+++ test/library_test.rb
@@ -53,4 +53,22 @@ class LibraryTest < Minitest::Test
 
     assert_equal 4, dpl.books.count
   end
+
+  def test_it_can_find_publication_date_range
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    require "pry";binding.pry
+
+    assert_instance_of Hash, dpl.publication_time_frame_for(harper_lee)
+    assert_instance_of Hash, dpl.publication_time_frame_for(charlotte_bronte)
+  end
+
 end

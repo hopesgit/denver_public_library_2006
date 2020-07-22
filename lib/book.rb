5d2484f1a2c64b6b01d5d808c4c175f2d09de7e4
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-22 12:47:37 -0600

    Update Book and BookTest with fix to attribute test and initialize

--- lib/book.rb
+++ lib/book.rb
@@ -7,6 +7,6 @@ class Book
   def initialize(details)
     @author = details[:author_first_name] + " " + details[:author_last_name]
     @title = details[:title]
-    @publication_year = details[:publication_year]
+    @publication_year = details[:publication_date][-4..-1]
   end
 end

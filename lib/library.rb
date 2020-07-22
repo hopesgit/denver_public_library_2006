0df2ff3ea0d623a0cded2c9a49fe3e95b17e7a64
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-22 14:04:41 -0600

    Update LibraryTest with books-related test and Library with new method

--- lib/library.rb
+++ lib/library.rb
@@ -1,16 +1,24 @@
 class Library
 
   attr_reader :name,
-              :authors,
-              :books
+              :authors
 
   def initialize(name)
-    @name  = name
+    @name    = name
     @authors = []
-    @books  =[]
+    @books   = []
   end
 
   def add_author(author)
     @authors << author
+  end
+
+  def books
+    @authors.each do |author|
+      author.books.each do |book|
+        @books << book
+      end
+    end
+    @books
   end
 end
--- test/library_test.rb
+++ test/library_test.rb
@@ -39,4 +39,18 @@ class LibraryTest < Minitest::Test
 
     assert_equal [charlotte_bronte, harper_lee], dpl.authors
   end
+
+  def test_it_has_books
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    assert_equal 4, dpl.books.count
+  end
 end
